1905aeaaf2e4c060e1de1b522d0be6b1613e69c1
Brian Diaz <bidiaz101@gmail.com>
2021-12-18 21:33:01 -0600

    added more content to all-foods page

--- client/src/components/FoodCard.js
+++ client/src/components/FoodCard.js
@@ -1,13 +1,29 @@
 import React,{ useState } from 'react'
 
-function FoodCard({ id, name, category, dayUntilExp, table, price, spoilage }) {
+function FoodCard({ id, name, category, daysUntilExp, table, price, spoilage }) {
     const [showDeets, setShowDeets] = useState(false)
+
+    const spoilageLis = spoilage.split(', ').map(item => {
+        if(!item) return <li key="honey">Never spoils</li>
+
+        const newItem = item.replace(item[0], item[0].toUpperCase())
+        return <li key={item}>{newItem}</li>
+    })
 
     return (
         <div>
             <h1>{name}</h1>
-            <h2>{category}</h2>
-            <h3>{spoilage}</h3>
+            <button onClick={() => setShowDeets(!showDeets)}>{showDeets ? "Hide Details" : "Show Details"}</button>
+            {showDeets? (
+                <>
+                    <p>Category: {category}</p>
+                    <p>Good for: ~{daysUntilExp} days</p>
+                    <p>Price Estimate: {price}</p>
+                    <div dangerouslySetInnerHTML={{__html: table}} />
+                    <h3>Signs of Spoilage</h3>
+                    <ul>{spoilageLis}</ul>
+                </>
+            ) : null}
         </div>
     )
 }
